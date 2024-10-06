2ca0e2de6dbd97254c67015d859b951b548eb7fd
K-C-PROF <161663939+K-C-PROF@users.noreply.github.com>
2024-10-06 04:28:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to display a welcome message
+function displayWelcomeMessage() {
+    alert("Welcome to University Solutions! We are here to help you with your work challenges."); // Displays a message in an alert box
+}
+
+
